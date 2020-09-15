22df72eb8df3d5705490ae44df5c1999041bc58d
ravihashi <ravicha_hashimoto@cmu.ac.th>
2020-09-16 00:12:40 +0700

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDNrTE-_uZpqjNj79zxMIyh03ZbjT5DiG0",
+  authDomain: "sunerry-6cc5e.firebaseapp.com",
+  databaseURL: "https://sunerry-6cc5e.firebaseio.com",
+  projectId: "sunerry-6cc5e",
+  storageBucket: "sunerry-6cc5e.appspot.com",
+  messagingSenderId: "730996422946",
+  appId: "1:730996422946:web:7e3e3a12e5965e00be8d7b",
+  measurementId: "G-M9DLY6F4ZM",
+};
